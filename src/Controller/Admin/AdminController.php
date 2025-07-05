b2d6e7df922c302ab6491cb0344bb0acbdb70882
Oscar Esteve <oscar@oscaresteve.com>
2025-07-05 13:04:47 +0100

    Refactor admin routing: rename `AgentController` to `AdminController`, update route and template structure.

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\AbstractController;
+use App\Repository\AgentRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin')]
+final class AdminController extends AbstractController
+{
+    #[Route('{path}', name: 'app_admin', requirements: ['path' => '.+'], defaults: ['path' => ''])]
+    public function index(): Response
+    {
+        return $this->render('admin/list.html.twig');
+    }
+}
