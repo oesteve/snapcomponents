c67d976cab3fe2b703afac2f8153c0915d87acc5
Oscar Esteve <oscar@oscaresteve.com>
2025-07-05 12:24:50 +0100

    Add `list` endpoint to `ProductController` and configure route format

--- src/Controller/API/Product/ProductController.php
+++ src/Controller/API/Product/ProductController.php
@@ -3,15 +3,27 @@
 namespace App\Controller\API\Product;
 
 use App\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use App\Service\Product\ProductSearchService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-#[Route('/api/products')]
+#[Route('/api/products', format: 'json')]
 class ProductController extends AbstractController
 {
+
+    #[Route('', methods: ['GET'])]
+    public function list(
+        ProductRepository $productRepository,
+    ): JsonResponse
+    {
+        return $this->json(
+            $productRepository->findAll(),
+        );
+    }
+
     #[Route('/search', methods: ['GET'])]
     public function search(
         ProductSearchService $productSearchService,
