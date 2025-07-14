00313f8508ab4f478791f2215eef04751367d829
Oscar Esteve <oscar@oscaresteve.com>
2025-07-14 08:43:48 +0100

    Refactor `EmbeddingsService` to initialize OpenAI client with DI attributes and support `ClientInterface`.

--- src/Service/Search/EmbeddingsService.php
+++ src/Service/Search/EmbeddingsService.php
@@ -3,14 +3,24 @@
 namespace App\Service\Search;
 
 use OpenAI\Client;
+use Psr\Http\Client\ClientInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 readonly class EmbeddingsService
 {
+    private Client $client;
+
     public function __construct(
-        private Client $client,
         private LoggerInterface $logger,
+        #[Autowire(env: 'OPENAI_SECRET_KEY')]
+        string $apiApiKey,
+        ClientInterface $httpClient,
     ) {
+        $this->client = \OpenAI::factory()
+            ->withApiKey($apiApiKey)
+            ->withHttpClient($httpClient)
+            ->make();
     }
 
     /**
